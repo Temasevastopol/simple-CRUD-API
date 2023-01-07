0938cc08a08e2b62be29decae11b0b789ea9df7c
Tema_sevastopol <temasebastopol@yandex.ru>
2023-01-07 17:04:49 +0300

    feat: add inteface

---
+++ src/category/repository.ts
@@ -0,0 +1,14 @@
+import { Category } from './category';
+
+const categories: Category[] = [
+    {
+        id: 1,
+        name: 'Category 1',
+        description: 'my test category 1'
+    },
+    {
+        id: 2,
+        name: 'Category 2',
+        description: 'my test category 2'
+    }
+]
